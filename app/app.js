7d27523cbd693762ccb11b2862022f67095d67ca
MichalMiekina <michal.miekina98@gmail.com>
2023-12-11 12:50:55 +0100

    init front index

--- app/app.js
+++ app/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+var path = require("path");
+var routes = require("./routes")
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const predictionRoutes = require('./routes/predictions');
@@ -10,6 +12,11 @@ app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
 app.use('/predictions', predictionRoutes);
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine","ejs")
+
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
