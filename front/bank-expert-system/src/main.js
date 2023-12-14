9b06b17dd61c77c7b12e0a12ce07e1ebf5131b72
MichalMiekina <michal.miekina98@gmail.com>
2023-12-14 14:21:06 +0100

    fix index init, mainly eslint corrections

---
+++ front/bank-expert-system/src/main.js
@@ -0,0 +1,19 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import HomeView from './views/HomeView.vue';
+
+const routes = [
+    { path: '/', component: HomeView },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
