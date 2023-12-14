12420cf80c3a848f926b43622ee1dac699c02f84
MichalMiekina <michal.miekina98@gmail.com>
2023-12-14 08:03:27 +0100

    Revert "init front index"
    This reverts commit 7d27523cbd693762ccb11b2862022f67095d67ca.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-var path = require("path");
-var routes = require("./routes")
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/auth');
 const predictionRoutes = require('./routes/predictions');
@@ -12,11 +10,6 @@ app.use(bodyParser.json());
 
 app.use('/auth', authRoutes);
 app.use('/predictions', predictionRoutes);
-
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine","ejs")
-
-app.use(routes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
