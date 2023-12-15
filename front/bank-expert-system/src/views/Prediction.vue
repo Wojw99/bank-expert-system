65f6a835e31fea63b2da5bc2e69cf3959a95939c
MichalMiekina <michal.miekina98@gmail.com>
2023-12-15 18:38:18 +0100

    build prediction and training subpages

--- front/bank-expert-system/src/views/Prediction.vue
+++ front/bank-expert-system/src/views/Prediction.vue
@@ -1,17 +1,94 @@
 <template>
-    <div>
-        <h1>prediction</h1>
+  <div :style="{ width: '100%', height: '100vh' }">
+    <h1>prediction</h1>
+
+    <div id="form">
+      <div v-for="(column, index) in csvColumns" :key="index" class="form-row">
+        <!-- eslint-disable-next-line vuejs-accessibility/label-has-for -->
+        <label :for="getColumnId(index)" class="label">{{ column }}</label>
+        <input
+          :type="getInputType(column)"
+          :id="getColumnId(index)"
+          v-model="inputValues[column]"
+          class="input"
+        />
+      </div>
+      <br>
+      <div class="button-container">
+        <button @click="SignIn">Enter</button>
+        <button @click="clearInputs">Clear All</button>
+      </div>
     </div>
+  </div>
 </template>
+
+<style scoped>
+.form-row {
+  margin-bottom: 10px; /* Adjust the margin as needed */
+}
+
+.label {
+  margin-right: 10px; /* Adjust the margin as needed */
+}
+
+.button-container {
+  margin-top: 20px; /* Adjust the margin as needed */
+}
+
+button {
+  margin-right: 40px; /* Adjust the margin as needed */
+}
+</style>
 
 <script>
 export default {
-  name: 'PredictionPanel', // Changed the component name
+  name: 'PredictionPanel',
   data() {
     return {
+      csvColumns: [
+        'age', 'job', 'marital', 'education', 'default',
+        'balance', 'housing', 'contact', 'day', 'month',
+        'duration', 'campaign', 'pdays', 'previous', 'poutcome', 'deposit',
+      ],
+      inputValues: {},
     };
   },
   methods: {
+    getColumnId(index) {
+      return `column_${index}`;
+    },
+    getInputType(column) {
+      // Mapping column names to input types
+      const columnTypeMap = {
+        age: 'number',
+        job: 'text',
+        marital: 'text',
+        education: 'text',
+        default: 'text',
+        balance: 'number',
+        housing: 'text',
+        contact: 'text',
+        day: 'number',
+        month: 'text',
+        duration: 'number',
+        campaign: 'number',
+        pdays: 'number',
+        previous: 'number',
+        poutcome: 'text',
+        deposit: 'text',
+      };
+
+      // Return the corresponding input type or default to 'text'
+      return columnTypeMap[column] || 'text';
+    },
+    SignIn() {
+      // Handle form submission
+      console.log('Form submitted with values:', this.inputValues);
+    },
+    clearInputs() {
+      // Clear all input values
+      this.inputValues = {};
+    },
   },
 };
 </script>
--- front/bank-expert-system/src/views/Training.vue
+++ front/bank-expert-system/src/views/Training.vue
@@ -1,17 +1,119 @@
 <template>
-    <div>
-        <h1>training</h1>
+  <div>
+    <h1>Training</h1>
+
+    <!-- File upload section -->
+    <div class="section">
+      <!-- eslint-disable-next-line vuejs-accessibility/label-has-for -->
+      <label
+        id="csvFileLabel"
+        for="fileInput"
+      >
+        Choose CSV File:
+      </label>
+      <div
+        class="file-input-label"
+        :for="selectedFile ? 'fileInputHasValue' : 'fileInput'"
+      >
+        <span v-if="selectedFile">{{ selectedFile.name }}</span>
+        <span v-else>Click to choose a file</span>
+        <input
+          type="file"
+          id="fileInput"
+          accept=".csv"
+          @change="handleFileUpload"
+          aria-label="Choose CSV File"
+        />
+      </div>
+      <button @click="startTraining">Start Training</button>
     </div>
+
+    <!-- Past and current effectiveness section -->
+    <div class="section">
+      <h2>Past and Current Effectiveness</h2>
+      <!-- Add your UI elements for past and current effectiveness here -->
+    </div>
+
+    <!-- Model overwrite UI element -->
+    <div class="section">
+      <!-- eslint-disable-next-line vuejs-accessibility/label-has-for -->
+      <label for="overwriteModel">Overwrite Existing Model:</label>
+      <div class="yes-no-buttons">
+        <button
+          @click="handleModelOverwrite(true)"
+          :class="{ active: overwriteModel }"
+        >
+          Yes
+        </button>
+        <button
+          @click="handleModelOverwrite(false)"
+          :class="{ active: !overwriteModel }"
+        >
+          No
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'TrainingPanel', // Changed the component name
+  name: 'TrainingPanel',
   data() {
     return {
+      selectedFile: null,
+      overwriteModel: false,
     };
   },
   methods: {
+    handleFileUpload(event) {
+      const [selectedFile] = event.target.files;
+      if (selectedFile) {
+        this.selectedFile = selectedFile;
+      }
+    },
+    startTraining() {
+      // Add logic to start training the model using this.selectedFile
+      if (this.selectedFile) {
+        console.log('Training started with file:', this.selectedFile);
+        console.log('Overwrite model:', this.overwriteModel);
+        // Add additional logic for training
+      } else {
+        console.error('No file selected for training.');
+      }
+    },
+    handleModelOverwrite(value) {
+      this.overwriteModel = value;
+    },
   },
 };
 </script>
+
+<style scoped>
+.section {
+  border: 1px solid #ddd;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+
+.file-input-label {
+  cursor: pointer;
+  display: inline-block;
+  margin-left: 10px;
+}
+
+.yes-no-buttons {
+  display: flex;
+}
+
+.yes-no-buttons button {
+  margin-right: 10px;
+  padding: 5px 10px;
+  cursor: pointer;
+}
+
+.yes-no-buttons button.active {
+  background-color: #4CAF50;
+  color: #fff;
+}
+</style>
